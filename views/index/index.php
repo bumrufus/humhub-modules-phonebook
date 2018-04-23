d8fb21f953fdd40d81baa4a3a20ae26f8bcaee83
bumrufus <34074733+bumrufus@users.noreply.github.com>
2018-04-23 12:24:55 +0200

    removed js and css
    and added it to resources

--- views/index/index.php
+++ views/index/index.php
@@ -38,50 +38,8 @@ $label_field7 = 'Position';
 $label_field8 = 'Abteilung';
 
 ?>
-<style>
-tr{border-left:3px solid white}
-table th{font-size:16px;text-align:center;padding-bottom:10px;}
-.tbody:hover {background-color:#f7f7f7;border-left:3px solid #e10000}
-</style>
-
-<script>
 
 
-function gi(name)
-{
-	return document.getElementById(name);
-}
-function filter_table() {
-  // Declare variables
-  var input, filter, table, tr, td, i;
-  input = gi("search");
-  filter = input.value.toLowerCase();
-  table = gi("table-data");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    tds = tr[i].getElementsByTagName("td");
-	var sh = "none";
-	var j;
-	for(j = 0; j < tds.length; j++)
-	{
-		if(tds[j])
-		{
-		  var str = tds[j].textContent || tds[j].innerHTML;
-		  if(str.toLowerCase().indexOf(filter) > -1)
-		  {
-			sh = "";
-			break;
-		  }
-		}
-	}
-	tr[i].style.display = sh;
-  }
-}
-
-
-</script>
 
 
 
