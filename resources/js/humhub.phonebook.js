1c92791a6f0e02a0fe69e99da8c4429608029596
bumrufus <34074733+bumrufus@users.noreply.github.com>
2018-04-23 12:25:55 +0200

    Update humhub.phonebook.js

--- resources/js/humhub.phonebook.js
+++ resources/js/humhub.phonebook.js
@@ -15,3 +15,37 @@ humhub.module('phonebook', function(module, require, $) {
         hello: hello
     });
 });
+
+function gi(name)
+{
+	return document.getElementById(name);
+}
+function filter_table() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = gi("search");
+  filter = input.value.toLowerCase();
+  table = gi("table-data");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    tds = tr[i].getElementsByTagName("td");
+	var sh = "none";
+	var j;
+	for(j = 0; j < tds.length; j++)
+	{
+		if(tds[j])
+		{
+		  var str = tds[j].textContent || tds[j].innerHTML;
+		  if(str.toLowerCase().indexOf(filter) > -1)
+		  {
+			sh = "";
+			break;
+		  }
+		}
+	}
+	tr[i].style.display = sh;
+  }
+}
+
