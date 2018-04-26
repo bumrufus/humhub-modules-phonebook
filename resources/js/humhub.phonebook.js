b2db41f9d501fc8669fc84269e2fbd1195291c17
Samuel Havel <34074733+bumrufus@users.noreply.github.com>
2018-04-26 10:49:30 +0200

    Update humhub.phonebook.js

--- resources/js/humhub.phonebook.js
+++ resources/js/humhub.phonebook.js
@@ -48,4 +48,9 @@ function filter_table() {
 	tr[i].style.display = sh;
   }
 }
-
+function stopEnterKey(evt) {
+        var evt = (evt) ? evt : ((event) ? event : null);
+        var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+        if ((evt.keyCode == 13) && (node.type == "text")) { return false; }
+    }
+    document.onkeypress = stopEnterKey;
