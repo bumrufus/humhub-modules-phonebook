e6fa26e6828a4155f2394138bf52323f6e934f1e
Samuel Havel <34074733+bumrufus@users.noreply.github.com>
2018-04-27 08:48:39 +0200

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="panel panel-default">
-        <div class="panel-heading"><strong>Phonebook</strong> <?= Yii::t("PhonebookModule.base", "configuration") ?></div>
+        <div class="panel-heading"><?= Yii::t("PhonebookModule.base", "<strong>Phonebook</strong> configuration") ?></div>
 
         <div class="panel-body">
             <p><?= Yii::t("PhonebookModule.base", "Welcome to the admin area. I want to add some settings, but i need more time!") ?></p>
