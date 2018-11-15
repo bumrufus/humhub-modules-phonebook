1fe2130c5af11744c0bcc53feaa98ffcf6eb30bb
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-11-15 21:19:45 +0100

    Fix Events errors
    https://github.com/humhub/humhub/issues/3363

--- config.php
+++ config.php
@@ -1,26 +1,12 @@
 <?php
 
+use humhub\widgets\TopMenu;
+
 return [
-	'id' => 'phonebook',
-	'class' => 'phonebook\humhub\modules\phonebook\Module',
-	'namespace' => 'phonebook\humhub\modules\phonebook',
-	'events' => [
-		[
-			'class' => \humhub\widgets\TopMenu::class,
-			'event' => \humhub\widgets\TopMenu::EVENT_INIT,
-			'callback' => ['phonebook\humhub\modules\phonebook\Events', 'onTopMenuInit'],
-		],
-//		[
-//			'class' => humhub\modules\admin\widgets\AdminMenu::class,
-//			'event' => humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-//			'callback' => ['phonebook\humhub\modules\phonebook\Events', 'onAdminMenuInit']
-//		],
-	],
-	'urlManagerRules' => [
-		'telefonliste' => 'phonebook/index'
-		 ]
-
+    'id' => 'phonebook',
+    'class' => 'humhub\modules\phonebook\Module',
+    'namespace' => 'humhub\modules\phonebook',
+    'events' => [
+        ['class' => TopMenu::class, 'event' => TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\phonebook\Events', 'onTopMenuInit']],
+    ],
 ];
- 
-?>
-
